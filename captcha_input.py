97100a11ed8f62ff29feedabe7e21f06005d73ab
Simon Turner <simonturner34@gmail.com>
2017-02-17 16:19:42 +0000

    Initial chunking attempt.

--- captcha_input.py
+++ captcha_input.py
@@ -6,25 +6,36 @@ import requests
 from PIL import Image
 import os
 import glob
-import cifar100nn
+import nn
 
 
 def find_image_url(captcha_iframe, image_checkboxes=None):
     print("Getting URLs.")
+
     if image_checkboxes:
         image_urls = []
         for checkbox in image_checkboxes:
+            # image_iframe = browser.find_by_css('body > div:nth-child(2) > div:nth-child(4) > iframe')
+            # captcha_iframe = browser.get_iframe(image_iframe.first['name'])
             x, y = checkbox['position']
+            print("checkbox at {0}, {1}".format(x,y))
             changed_image_xpath = '//*[@id="rc-imageselect-target"]/table/tbody/tr[{0}]/td[{1}]/div/div[1]/img'\
                 .format(x, y)
             if captcha_iframe.is_element_present_by_xpath(changed_image_xpath, wait_time=3):
                 image_url = captcha_iframe.find_by_xpath(changed_image_xpath)['src']
+                print("found image url")
                 image_urls.append(image_url)
+            else:
+                print("can't find image")
         return image_urls
     else:
+        # image_iframe = browser.find_by_css('body > div:nth-child(2) > div:nth-child(4) > iframe')
+        # captcha_iframe = browser.get_iframe(image_iframe.first['name'])
         image_xpath = '//*[@id="rc-imageselect-target"]/table/tbody/tr[1]/td[1]/div/div[1]/img'
         if captcha_iframe.is_element_present_by_xpath(image_xpath, wait_time=3):
             image_url = captcha_iframe.find_by_xpath(image_xpath)['src']
+        else:
+            print("can't find image")
         return image_url
 
 
@@ -69,21 +80,21 @@ def get_image_checkboxes(rows, cols, captcha_iframe):
     return image_checkboxes
 
 
-def verify(captcha_iframe, correct_score, total_guesses):
+def verify(captcha_iframe):
     print("Clicking verify.")
     if captcha_iframe.is_element_present_by_id('recaptcha-verify-button', wait_time=3):
         verify_button = captcha_iframe.find_by_id('recaptcha-verify-button')
         verify_button.first.click()
 
-    not_select_all_images_error = captcha_iframe.is_text_not_present('Please select all matching images.')
-    not_retry_error = captcha_iframe.is_text_not_present('Please try again.')
-    not_select_more_images_error = captcha_iframe.is_text_not_present('Please also check the new images.')
-    if not_select_all_images_error and not_retry_error and not_select_more_images_error:
-        correct_score += 1
-    total_guesses += 1
-    print("Total possibly correct: {correct}".format(correct=correct_score))
-    print("Total guesses: {guesses}".format(guesses=total_guesses))
-    print("Percentage: {percent}".format(percent=float(correct_score)/total_guesses))
+    # not_select_all_images_error = captcha_iframe.is_text_not_present('Please select all matching images.')
+    # not_retry_error = captcha_iframe.is_text_not_present('Please try again.')
+    # not_select_more_images_error = captcha_iframe.is_text_not_present('Please also check the new images.')
+    # if not_select_all_images_error and not_retry_error and not_select_more_images_error:
+    #     correct_score += 1
+    # total_guesses += 1
+    # print("Total possibly correct: {correct}".format(correct=correct_score))
+    # print("Total guesses: {guesses}".format(guesses=total_guesses))
+    # print("Percentage: {percent}".format(percent=float(correct_score)/total_guesses))
 
 
 def delete_old_images():
@@ -125,7 +136,6 @@ def resize_images():
 
 
 def find_rows_and_cols(captcha_iframe):
-    time.sleep(1)
     rows = captcha_iframe.find_by_xpath('//*[@id="rc-imageselect-target"]/table/tbody/child::tr')
     cols = captcha_iframe.find_by_xpath('//*[@id="rc-imageselect-target"]/table/tbody/tr[1]/child::td')
     print("rows from find_rows_and_cols: {0}, cols: {1}".format(len(rows), len(cols)))
@@ -140,8 +150,17 @@ def get_captcha_query(captcha_iframe):
 
 
 def pick_checkboxes_matching_query(image_checkboxes, predicted_word_labels, query):
-    matching_labels = [i for i in range(len(predicted_word_labels)) if predicted_word_labels[i] == query]
+    matching_labels = []
+    for i, image_labels in enumerate(predicted_word_labels):
+        for j, label in enumerate(image_labels):
+            if image_labels[j] == query:
+                matching_labels.append(i) # this works if the test is == because there aren't duplicate labels
+        # for i in range(len(image_labels)):
+        #     if image_labels[i] == query:
+        #         matching_labels.append(i * len(predicted_word_labels) + j)
+
     print(predicted_word_labels, query)
+    print(matching_labels)
     matching_image_checkboxes = pick_checkboxes_from_positions(matching_labels, image_checkboxes)
     return matching_image_checkboxes
 
@@ -167,94 +186,99 @@ def click_initial_checkbox():
 
 
 def guess_captcha(browser):
-    if browser.is_element_present_by_css('body > div > div:nth-child(4) > iframe', wait_time=3):
-        image_iframe = browser.find_by_css('body > div > div:nth-child(4) > iframe')
 
-        correct_score = 0
-        total_guesses = 0  # not necessarily separate captchas, one captcha with new images added would count as two
-        # image_checkboxes = captcha_iframe.find_by_xpath('//div[@class="rc-imageselect-checkbox"]')
+    correct_score = 0
+    total_guesses = 0  # not necessarily separate captchas, one captcha with new images added would count as two
+    # image_checkboxes = captcha_iframe.find_by_xpath('//div[@class="rc-imageselect-checkbox"]')
 
-        new_run = True
-        while True:
-            if browser.is_element_present_by_css('body > div > div:nth-child(4) > iframe', wait_time=3):
-                with browser.get_iframe(image_iframe.first['name']) as captcha_iframe:
-                    rows, cols = find_rows_and_cols(captcha_iframe)
-                    row_count = len(rows)
-                    col_count = len(cols)
+    new_run = True
+    while True:
+        if browser.is_element_present_by_css('.recaptcha-checkbox-checkmark', wait_time=3):
+            browser.reload()
+            correct_score += 1
+            print("Captchas Correct: {0}".format(correct_score))
+        elif browser.is_element_present_by_css('body > div > div:nth-child(4) > iframe', wait_time=3):
+            image_iframe = browser.find_by_css('body > div > div:nth-child(4) > iframe')
+            with browser.get_iframe(image_iframe.first['name']) as captcha_iframe:
+                rows, cols = find_rows_and_cols(captcha_iframe)
+                row_count = len(rows)
+                col_count = len(cols)
 
-                    # need to keep getting images and image urls until this batch of image urls is the same as the last run
-                    # i.e. keep selecting images until the captcha stops replacing images
-                    # checkbox_xpath = '//*[@id="rc-imageselect-target"]/table/tbody/tr[1]/td[1]/div'
+                # need to keep getting images and image urls until this batch of image urls is the same as the last run
+                # i.e. keep selecting images until the captcha stops replacing images
+                # checkbox_xpath = '//*[@id="rc-imageselect-target"]/table/tbody/tr[1]/td[1]/div'
 
-                    # if captcha_iframe.is_element_not_present_by_xpath(checkbox_xpath, wait_time=3):
-                    #     print("Clicking reload because captcha iframe cannot be found.")
-                    #     recaptcha_reload_button = captcha_iframe.find_by_id('recaptcha-reload-button')
-                    #     recaptcha_reload_button.click()
-                    #     continue
+                # if captcha_iframe.is_element_not_present_by_xpath(checkbox_xpath, wait_time=3):
+                #     print("Clicking reload because captcha iframe cannot be found.")
+                #     recaptcha_reload_button = captcha_iframe.find_by_id('recaptcha-reload-button')
+                #     recaptcha_reload_button.click()
+                #     continue
 
-                    # if new captcha, get checkboxes, download images, pick checkboxes
-                    if new_run:
-                        total_guesses = 0
-                        print("New CAPTCHA.")
-                        image_url = find_image_url(captcha_iframe)
-                        download_images(image_url, row_count, col_count)
-                        resize_images()
-                        predicted_word_labels = cifar100nn.convert_labels_to_label_names(cifar100nn.predict_image_classes())
-                        captcha_text = get_captcha_query(captcha_iframe)
-                        time.sleep(1)
-                        image_checkboxes = get_image_checkboxes(rows, cols, captcha_iframe)
-                        picked_checkboxes = pick_checkboxes_matching_query(image_checkboxes, predicted_word_labels, captcha_text)
 
-                        print(predicted_word_labels)
+                # if new captcha, get checkboxes, download images, pick checkboxes
+                if new_run:
+                    total_guesses = 0
+                    print("New CAPTCHA.")
+                    image_url = find_image_url(captcha_iframe)
+                    download_images(image_url, row_count, col_count)
+                    resize_images()
+                    predicted_word_labels = nn.convert_labels_to_label_names(nn.predict_image_classes())
+                    captcha_text = get_captcha_query(captcha_iframe)
+                    image_checkboxes = get_image_checkboxes(rows, cols, captcha_iframe)
+                    picked_checkboxes = pick_checkboxes_matching_query(image_checkboxes, predicted_word_labels, captcha_text)
 
-                        if not picked_checkboxes or total_guesses >= 4:
-                            reload(captcha_iframe)
-                            new_run = True
-                        else:
-                            click_checkboxes(picked_checkboxes)
+                    print(predicted_word_labels)
 
-                            # connection_alert = browser.get_alert()
-                            # if connection_alert:
-                            #     connection_alert.accept()
-                            #     click_initial_checkbox()
-
-                            total_guesses += 1
-                            new_run = False
-
-                        new_image_urls = find_image_url(captcha_iframe, image_checkboxes)
-
-                    elif any(image_url != new_image_url for new_image_url in new_image_urls):
-                        print("Some images have changed but CAPTCHA hasn't.")
-
-                        download_images(image_url, row_count, col_count, new_image_urls)
-
-                        captcha_text = get_captcha_query(captcha_iframe)
-                        resize_images()
-                        predicted_word_labels = cifar100nn.convert_labels_to_label_names(cifar100nn.predict_image_classes())
-                        time.sleep(1)
-                        new_image_checkboxes = get_image_checkboxes(rows, cols, captcha_iframe)
-                        picked_checkboxes = pick_checkboxes_matching_query(new_image_checkboxes, predicted_word_labels, captcha_text)
-                        # picked_checkboxes = pick_checkboxes_from_positions(picked_positions, new_image_checkboxes)
-                        print(predicted_word_labels)
-
-                        if picked_checkboxes:
-                            click_checkboxes(picked_checkboxes)
-
-                            # connection_alert = browser.get_alert()
-                            # if connection_alert:
-                            #     connection_alert.accept()
-                            #     click_initial_checkbox()
-
-                            total_guesses += 1
-                        else:
-                            verify(captcha_iframe, correct_score, total_guesses)
-                            new_run = True
-
-                        image_url = find_image_url(captcha_iframe)
-                        new_image_urls = find_image_url(captcha_iframe, new_image_checkboxes)
-                    else:
-                        verify(captcha_iframe, correct_score, total_guesses)
+                    if not picked_checkboxes or total_guesses >= 4:
+                        reload(captcha_iframe)
                         new_run = True
+                    else:
+                        click_checkboxes(picked_checkboxes)
+
+                        # connection_alert = browser.get_alert()
+                        # if connection_alert:
+                        #     connection_alert.accept()
+                        #     click_initial_checkbox()
+
+                        total_guesses += 1
+                        new_run = False
+
+                    new_image_urls = find_image_url(captcha_iframe, image_checkboxes)
+
+                elif any(image_url != new_image_url for new_image_url in new_image_urls):
+                    print("Some images have changed but CAPTCHA hasn't.")
+
+                    image_url = find_image_url(captcha_iframe)
+                    download_images(image_url, row_count, col_count, new_image_urls)
+
+                    captcha_text = get_captcha_query(captcha_iframe)
+                    resize_images()
+                    predicted_word_labels = nn.convert_labels_to_label_names(nn.predict_image_classes())
+                    new_image_checkboxes = get_image_checkboxes(rows, cols, captcha_iframe)
+                    picked_checkboxes = pick_checkboxes_matching_query(new_image_checkboxes, predicted_word_labels, captcha_text)
+                    # picked_checkboxes = pick_checkboxes_from_positions(picked_positions, new_image_checkboxes)
+                    print(predicted_word_labels)
+
+                    if picked_checkboxes:
+                        click_checkboxes(picked_checkboxes)
+
+                        # connection_alert = browser.get_alert()
+                        # if connection_alert:
+                        #     connection_alert.accept()
+                        #     click_initial_checkbox()
+
+                        total_guesses += 1
+                    else:
+                        verify(captcha_iframe)
+                        new_run = True
+
+                    new_image_urls = find_image_url(captcha_iframe, new_image_checkboxes)
+                elif all(image_url == new_image_url for new_image_url in new_image_urls):
+                    reload(captcha_iframe)
+                    new_run = True
+                else:
+                    verify(captcha_iframe)
+                    new_run = True
 
 
 with Browser() as browser:
--- image-preprocessing.py
+++ image-preprocessing.py
@@ -1,4 +1,3 @@
-import scipy.io
 import keras
 from keras.datasets import cifar100
 import numpy as np
@@ -7,99 +6,185 @@ import glob
 import os
 from PIL import Image
 from sklearn.cross_validation import train_test_split
+import math
+import re
 
 
 def resize_images(path):
     for infile in glob.glob(path):
-        file, ext = os.path.splitext(infile)
-        image = Image.open(infile)
-        image = image.resize((32, 32))
-        image.save(file + "_32x32" + ext)
+        if os.path.isfile(infile):
+            filename, ext = os.path.splitext(infile)
+            if "_32x32" not in filename:
+                print("resizing: {0}".format(filename + ext))
+                image = Image.open(infile)
+                image = image.resize((110, 110))
+                image.save(filename + "_110x110" + ext)
 
 
 def colour_images(path):
     for infile in glob.glob(path):
-        file, ext = os.path.splitext(infile)
-        image = Image.open(infile)
-        if image.mode != "RGB":
-            image = image.convert("RGB")
-            image.save(file + ext)
+        if os.path.isfile(infile):
+            filename, ext = os.path.splitext(infile)
+            if "_110x110" in filename:
+                print("opening: {0}".format(filename + ext))
+                image = Image.open(infile)
+                if image.mode != "RGB":
+                    print("image not RGB, colouring")
+                    image = image.convert("RGB")
+                    image.save(filename + ext)
 
 
-def create_labels_from_directories(path, num_images):
-    num_dirs = len(os.listdir(path))
+def create_labels_from_directories(paths, num_images):
     labels = np.zeros(shape=(num_images,), dtype='uint8')
     file_nums_list = []
-    for i, directory in enumerate(os.listdir(path)):
-        num_files = len([name for name in os.listdir(os.path.join(path, directory)) if os.path.isfile(os.path.join(path, directory, name)) and "_32x32" in name])
+    for j, path in enumerate(paths):
+        for i, directory in enumerate(os.listdir(path)):
+            num_files = len([name for name in os.listdir(os.path.join(path, directory)) if os.path.isfile(os.path.join(path, directory, name)) and "_110x110" in name])
 
-        # dir_labels = np.ndarray(shape=(num_files,), dtype='uint8')
-        # dir_labels.fill(i)
-        # print(dir_labels.shape, dir_labels)
-        if i == 0:
-            labels[:num_files] = i
-            file_nums_list.append(num_files)
-            # print(labels)
-        else:
-            start = np.sum(file_nums_list)
-            file_nums_list.append(num_files)
-            end = np.sum(file_nums_list)
-            labels[start:end] = i
-            print(i)
-            print(labels[:start])
-            # print(labels)
+            # dir_labels = np.ndarray(shape=(num_files,), dtype='uint8')
+            # dir_labels.fill(i)
+            # print(dir_labels.shape, dir_labels)
+            if j == 0:
+                labels[:num_files] = j
+
+            if i == 0:
+                file_nums_list.append(num_files)
+                # print(labels)
+            else:
+                start = np.sum(file_nums_list)
+                file_nums_list.append(num_files)
+                end = np.sum(file_nums_list)
+                labels[start:end] = i
+                # print(labels)
     total_current_labels = np.sum(file_nums_list)
-    print(len(np.unique(labels)))
-    return labels, total_current_labels, num_dirs
+    current_num_classes = len(np.unique(labels))
+    return labels, total_current_labels, current_num_classes
 
-
-# resize_images('datasets/selected_images/**/*')
+# print("resizing images")
+# resize_images('../../datasets/svhn-train/*.png')
+# resize_images('../../datasets/svhn-test/*.png')
+# resize_images('../../datasets/trafficsigns-train/**/*.ppm')
+# resize_images('../../datasets/trafficsigns-test/*.ppm')
 # print("colouring images")
-# colour_images('datasets/selected_images/**/*')
+# colour_images('../../datasets/svhn-train/*.png')
+# colour_images('../../datasets/svhn-test/*.png')
+# colour_images('../../datasets/trafficsigns-train/**/*.ppm')
+# colour_images('../../datasets/trafficsigns-test/*.ppm')
 
-print("loading main image set")
-outdoor_images = skimage.io.imread_collection('datasets/selected_images/**/*_32x32.jpg')
-print(outdoor_images[0].shape)
 
-print("making labels")
-labels, total_current_labels, current_num_classes = create_labels_from_directories('datasets/selected_images', len(outdoor_images))
-print(len(np.unique(labels)))
+def chunk_image_data(desired_num_chunks):
+    print("loading data")
+    places2 = skimage.io.imread_collection('../../Downloads/train_places365/**/**/*_110x110.jpg')
+    print("creating labels")
+    labels, total_current_labels, num_classes = create_labels_from_directories(glob.glob('../../Downloads/train_places365/**'), len(places2))
+    print(labels.shape, num_classes)
 
-print("loading svhn")
-svhn_train = skimage.io.imread_collection('datasets/svhn-train/*_32x32.png')
-svhn_test = skimage.io.imread_collection('datasets/svhn-test/*_32x32.png')
-svhn_train_labels = np.ndarray(shape=(len(svhn_train),), dtype='uint8')
-svhn_train_labels.fill(current_num_classes + 1)
-svhn_test_labels = np.ndarray(shape=(len(svhn_test),), dtype='uint8')
-svhn_test_labels.fill(current_num_classes + 1)
-print(svhn_train[0].shape)
+    places2_length = len(places2)
+    print(places2_length)
+    if places2_length % 2 == 0:
+        step = 2
+    else:
+        step = 1
+    data_length_factors = [x for x in range(1, int(math.sqrt(places2_length))+1, step) if places2_length % x == 0]
+    print(data_length_factors)
+    desired_chunk_divisor = desired_num_chunks
+    chunk_divisor = math.inf
+    previous_difference = math.inf
+    for factor in data_length_factors:
+        if abs(factor - desired_chunk_divisor) < previous_difference:
+            chunk_divisor = factor
+        previous_difference = abs(factor - desired_chunk_divisor)
 
-print("loading traffic signs")
-traffic_signs_train = skimage.io.imread_collection('datasets/trafficsigns-train/**/*_32x32.ppm')
-traffic_signs_test = skimage.io.imread_collection('datasets/trafficsigns-test/*_32x32.ppm')
-traffic_signs_train_labels = np.ndarray(shape=(len(traffic_signs_train),), dtype='uint8')
-traffic_signs_train_labels.fill(current_num_classes + 2)
-traffic_signs_test_labels = np.ndarray(shape=(len(traffic_signs_test),), dtype='uint8')
-traffic_signs_test_labels.fill(current_num_classes + 2)
-print(traffic_signs_train[0].shape)
+    print(chunk_divisor)
+    # find factors of the length, and then find the closest factor to a divisor that will chunk the data into small enough
+    # chunks - this way we get an exact divisor of the dataset, rather than missing out images because of truncation.
 
-print("splitting train and test sets")
-train, test, labels_train, labels_test = train_test_split(outdoor_images, labels, test_size=0.2, random_state=123412)
+    chunk_size = int(len(places2) / chunk_divisor)
 
-print("concatenating")
-train = np.concatenate((train, svhn_train, traffic_signs_train), axis=0)
-labels_train = np.concatenate((labels_train, svhn_train_labels, traffic_signs_train_labels), axis=0)
-test = np.concatenate((test, svhn_test, traffic_signs_test), axis=0)
-test_labels = np.concatenate((labels_test, svhn_test_labels, traffic_signs_test_labels), axis=0)
+    # completed_chunks = []
+    # for file in os.listdir('.'):
+    #     if os.path.isfile(file) and "chunk_" in file:
+    #         chunk_number_pos = re.search(r'[0-9]', file).start()
+    #         completed_chunk_num = file[chunk_number_pos:chunk_number_pos+1]
+    #     if completed_chunk_num not in completed_chunks:
+    #         completed_chunks.append(completed_chunk_num)
 
-print("creating validation sets")
-train, validation, labels_train, labels_validation = train_test_split(train, labels_train, test_size=0.2, random_state=34565)
+    # find the chunk files we've already finished,
 
-print(train.shape, test.shape, validation.shape, labels_train.shape, labels_test.shape, labels_validation.shape)
-print("saving files")
-np.save('datasets/train.npy', train)
-np.save('datasets/train_labels.npy', labels_train)
-np.save('datasets/test.npy', test)
-np.save('datasets/test_labels.npy', labels_test)
-np.save('datasets/validation.npy', validation)
-np.save('datasets/validation_labels.npy', labels_validation)
+
+    chunk_number = 0
+    print("Beginning chunking with chunk size: {0}".format(chunk_size))
+    for i in range(0, len(places2), chunk_size):
+        print("Chunk number: {0}".format(chunk_number))
+        print("Slicing image data.")
+        chunk = places2[i:i+chunk_size].concatenate()
+        print(chunk.shape)
+        print("Slicing labels.")
+        chunk_labels = labels[i:i+chunk_size]
+        print(chunk_labels.shape)
+        print("Splitting train and test sets.")
+        train, test, train_labels, test_labels = train_test_split(chunk, chunk_labels, test_size=0.1, random_state=43)
+        print("Splitting train and validation sets.")
+        train, validation, train_labels, validation_labels = train_test_split(train, train_labels, test_size=0.1, random_state=234)
+
+        print("Saving train, test and validation.")
+        np.save("chunk_{0}_train.npy".format(chunk_number), train)
+        np.save("chunk_{0}_test.npy".format(chunk_number), test)
+        np.save("chunk_{0}_validation.npy".format(chunk_number), validation)
+
+        print("Saving labels.")
+        np.save("chunk_{0}_train_labels.npy".format(chunk_number), train_labels)
+        np.save("chunk_{0}_test_labels.npy".format(chunk_number), test_labels)
+        np.save("chunk_{0}_validation_labels.npy".format(chunk_number), validation_labels)
+        # train test split, with labels
+        # train validation split, with labels
+        # save files, using chunk number
+        print(train.shape, test.shape, validation.shape, train_labels.shape, test_labels.shape, validation_labels.shape)
+        chunk_number += 1
+
+chunk_image_data(137)
+
+# print("loading main image set")
+# outdoor_images = skimage.io.imread_collection('datasets/selected_images/**/*_32x32.jpg')
+# print(outdoor_images[0].shape)
+#
+# print("making labels")
+# labels, total_current_labels, current_num_classes = create_labels_from_directories('datasets/selected_images', len(outdoor_images))
+# print(len(np.unique(labels)))
+#
+# print("loading svhn")
+# svhn_train = skimage.io.imread_collection('datasets/svhn-train/*_32x32.png')
+# svhn_test = skimage.io.imread_collection('datasets/svhn-test/*_32x32.png')
+# svhn_train_labels = np.ndarray(shape=(len(svhn_train),), dtype='uint8')
+# svhn_train_labels.fill(current_num_classes + 1)
+# svhn_test_labels = np.ndarray(shape=(len(svhn_test),), dtype='uint8')
+# svhn_test_labels.fill(current_num_classes + 1)
+# print(svhn_train[0].shape)
+#
+# print("loading traffic signs")
+# traffic_signs_train = skimage.io.imread_collection('datasets/trafficsigns-train/**/*_32x32.ppm')
+# traffic_signs_test = skimage.io.imread_collection('datasets/trafficsigns-test/*_32x32.ppm')
+# traffic_signs_train_labels = np.ndarray(shape=(len(traffic_signs_train),), dtype='uint8')
+# traffic_signs_train_labels.fill(current_num_classes + 2)
+# traffic_signs_test_labels = np.ndarray(shape=(len(traffic_signs_test),), dtype='uint8')
+# traffic_signs_test_labels.fill(current_num_classes + 2)
+# print(traffic_signs_train[0].shape)
+#
+# print("concatenating")
+# data = np.concatenate((outdoor_images, svhn_train, traffic_signs_train, svhn_test, traffic_signs_test), axis=0)
+# labels = np.concatenate((labels, svhn_train_labels, traffic_signs_train_labels, svhn_test_labels, traffic_signs_test_labels), axis=0)
+#
+# print("splitting train and test sets")
+# train, test, labels_train, labels_test = train_test_split(data, labels, test_size=0.2, random_state=123412)
+#
+# print("creating validation sets")
+# train, validation, labels_train, labels_validation = train_test_split(train, labels_train, test_size=0.2, random_state=34565)
+#
+# print(train.shape, test.shape, validation.shape, labels_train.shape, labels_test.shape, labels_validation.shape)
+# print("saving files")
+# np.save('datasets/train.npy', train)
+# np.save('datasets/train_labels.npy', labels_train)
+# np.save('datasets/test.npy', test)
+# np.save('datasets/test_labels.npy', labels_test)
+# np.save('datasets/validation.npy', validation)
+# np.save('datasets/validation_labels.npy', labels_validation)
